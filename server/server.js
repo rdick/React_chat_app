f7928589a07b9f8c19d255747eb3cb54700bcb82
Ryan Dick <rdick@ualberta.ca>
2020-05-27 07:19:13 -0700

    added server and react files and dependencies. Also created server in server.js.

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io")
+const io = socket(server)
+
+server.listen(8000, () =>
+    console.log('Server is running on Port 8000')
+)
