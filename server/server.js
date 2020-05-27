acc7c2b2c25c450e14c5883177d34b52faff3ebc
Ryan Dick <rdick@ualberta.ca>
2020-05-27 09:02:30 -0700

    finished chat_app - one bug

--- server/server.js
+++ server/server.js
@@ -5,6 +5,13 @@ const server = http.createServer(app);
 const socket = require("socket.io")
 const io = socket(server)
 
+io.on("connection", socket => {
+    socket.emit("your id", socket.id)
+    socket.on("send message", body => {
+        io.emit("message", body)
+    })
+})
+
 server.listen(8000, () =>
     console.log('Server is running on Port 8000')
 )
